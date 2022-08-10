0381bbfd5b16133c59de7c2cebc197ebcfe48c1e
Carlos Roberto Catarino Neto <94271854+carlosrcneto@users.noreply.github.com>
2022-08-10 10:33:39 -0300

    Game Mata mosquito continuação

--- JavaScript/app2/jogo.js
+++ JavaScript/app2/jogo.js
@@ -1,5 +1,6 @@
 var altura = 0
 var largura = 0
+var vidas = 1
 
 function ajustaTamanhoPalcoJogo() {
     altura = window.innerHeight
@@ -12,6 +13,18 @@ ajustaTamanhoPalcoJogo()
 
 function posicaoRandomica() {
 
+    //remover o mosquito anterior (caso exista)
+    if(document.getElementById('mosquito')) {
+        document.getElementById('mosquito').remove()
+
+        if(vidas > 3) {
+            alert('Imterromper o jogo')
+        } else {
+            document.getElementById('v' + vidas).src="imagens/coracao_vazio.png"
+            vidas ++
+        }
+    }
+
     var posicaoX = Math.floor(Math.random() * largura) - 90
     var posicaoY = Math.floor(Math.random() * altura) - 90
 
@@ -23,10 +36,41 @@ function posicaoRandomica() {
     //criar o elemento html
     var mosquito = document.createElement('img')
     mosquito.src = 'imagens/mosquito.png'
-    mosquito.className = "mosquito1"
+    mosquito.className = tamanhoAleatorio() + ' ' + ladoAleatorio()
     mosquito.style.left = posicaoX + 'px'
     mosquito.style.top = posicaoY + 'px'
     mosquito.style.position = 'absolute'
+    mosquito.id = 'mosquito'
+    mosquito.onclick = function() {
+        this.remove()
+    }
 
     document.body.appendChild(mosquito)
+}
+
+function tamanhoAleatorio() {
+    var classe = Math.floor(Math.random() * 3)
+
+    switch(classe){
+        case 0:
+            return 'mosquito1'
+
+        case 1:
+            return 'mosquito2'
+
+        case 2:
+            return 'mosquito3'
+    }
+}
+
+function ladoAleatorio() {
+    var classe = Math.floor(Math.random() * 2)
+
+    switch(classe){
+        case 0:
+            return 'ladoA'
+
+        case 1:
+            return 'ladoB'
+    }
 }
